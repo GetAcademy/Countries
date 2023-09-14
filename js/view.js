99d88c32c52248cef1f749d42b0ec2013491d5d4
GET Academy <terje@kolderup.net>
2023-09-14 12:59:11 +0200

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,8 +1,14 @@
 "use strict";
 
 function updateView() {
+    let countriesHtml = '';
+    for(let country of model.costStats){
+        countriesHtml += `<li>${country.country} øl: ${country.beer}kr</li>`;
+    }
+
     document.getElementById('app').innerHTML = /*HTML*/`
 
+        <h1>Pause til 13:05</h1>
+        ${countriesHtml}
     `;
-    
 }
